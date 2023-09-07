5ea85e37350dc3668750cf35417db4e48c1568f3
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2023-09-07 18:32:10 +0600

    add bean for create and обмен очередями

--- src/main/java/com/zhassurbek/mixproject/config/RabbitMQConfig.java
+++ src/main/java/com/zhassurbek/mixproject/config/RabbitMQConfig.java
@@ -2,6 +2,9 @@ package com.zhassurbek.mixproject.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -34,5 +37,20 @@ public class RabbitMQConfig {
         rabbitTemplate.setMessageConverter(converter);
         return rabbitTemplate;
     }
+
+    @Bean
+    public Declarables createPostRegistrationSchema() {
+        return new Declarables(
+                // объявляем обмен, по соглашению название бирж начинается с x
+                new FanoutExchange("x.post-registration"),
+                // объевляем очереди
+                new Queue("q.send-email"),
+                new Queue("q.send-sms"),
+                // объявили привязку очередей к этому обмену x.post-registration
+                new Binding("q.send-email", Binding.DestinationType.QUEUE, "x.post-registration", "send-email", null),
+                new Binding("q.send-sms", Binding.DestinationType.QUEUE, "x.post-registration", "send-sms", null)
+        );
+    }
+
 
 }
