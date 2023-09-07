bcc09c37a42ae7fce034d85e14c6a17e447518c5
zhassurbek <zhassurbek.boltabayev.2000@gmail.com>
2023-09-07 17:53:35 +0600

    add converter from base64 to JSON

--- src/main/java/com/zhassurbek/mixproject/config/RabbitMQConfig.java
+++ src/main/java/com/zhassurbek/mixproject/config/RabbitMQConfig.java
@@ -1,16 +1,38 @@
 package com.zhassurbek.mixproject.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@Slf4j
+@RequiredArgsConstructor
 public class RabbitMQConfig {
+
+    private final CachingConnectionFactory cachingConnectionFactory;
 
     // import Queue from amqp
     @Bean
     public Queue createUserRegistrationQueue() {
         return new Queue("q.user-registration");
     }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory);
+        rabbitTemplate.setMessageConverter(converter);
+        return rabbitTemplate;
+    }
+
 }
